620eb0e008c94fac0e78a95012ebc529cadb2622
iptommy0125 <iptommy0125@gmail.com>
2022-08-14 15:23:06 +0800

    preflight check 3+4

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -33,7 +33,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         gamelib.debug_write('Configuring your custom algo strategy...')
         self.config = config
-        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP, ENEMY_MAP
+        global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP, ENEMY_MAP, FRIENDLY_MAP
         WALL = config["unitInformation"][0]["shorthand"]
         SUPPORT = config["unitInformation"][1]["shorthand"]
         TURRET = config["unitInformation"][2]["shorthand"]
@@ -42,6 +42,11 @@ class AlgoStrategy(gamelib.AlgoCore):
         INTERCEPTOR = config["unitInformation"][5]["shorthand"]
         ENEMY_MAP = [[13, 27], [14, 27], [12, 26], [13, 26], [14, 26], [15, 26], [11, 25], [12, 25], [13, 25], [14, 25], [15, 25], [16, 25], [10, 24], [11, 24], [12, 24], [13, 24], [14, 24], [15, 24], [16, 24], [17, 24], [9, 23], [10, 23], [11, 23], [12, 23], [13, 23], [14, 23], [15, 23], [16, 23], [17, 23], [18, 23], [8, 22], [9, 22], [10, 22], [11, 22], [12, 22], [13, 22], [14, 22], [15, 22], [16, 22], [17, 22], [18, 22], [19, 22], [7, 21], [8, 21], [9, 21], [10, 21], [11, 21], [12, 21], [13, 21], [14, 21], [15, 21], [16, 21], [17, 21], [18, 21], [19, 21], [20, 21], [6, 20], [7, 20], [8, 20], [9, 20], [10, 20], [11, 20], [12, 20], [13, 20], [14, 20], [15, 20], [16, 20], [17, 20], [18, 20], [19, 20], [20, 20], [21, 20], [5, 19], [6, 19], [7, 19], [8, 19], [9, 19], [10, 19], [11, 19], [12, 19], [13, 19], [14, 19], [15, 19], [16, 19], [17, 19], [18, 19], [19, 19], [20, 19], [21, 19], [22, 19], [4, 18], [5, 18], [6, 18], [7, 18], [8, 18], [9, 18], [10, 18], [11, 18], [12, 18], [13, 18], [14, 18], [15, 18], [16, 18], [17, 18], [
             18, 18], [19, 18], [20, 18], [21, 18], [22, 18], [23, 18], [3, 17], [4, 17], [5, 17], [6, 17], [7, 17], [8, 17], [9, 17], [10, 17], [11, 17], [12, 17], [13, 17], [14, 17], [15, 17], [16, 17], [17, 17], [18, 17], [19, 17], [20, 17], [21, 17], [22, 17], [23, 17], [24, 17], [2, 16], [3, 16], [4, 16], [5, 16], [6, 16], [7, 16], [8, 16], [9, 16], [10, 16], [11, 16], [12, 16], [13, 16], [14, 16], [15, 16], [16, 16], [17, 16], [18, 16], [19, 16], [20, 16], [21, 16], [22, 16], [23, 16], [24, 16], [25, 16], [1, 15], [2, 15], [3, 15], [4, 15], [5, 15], [6, 15], [7, 15], [8, 15], [9, 15], [10, 15], [11, 15], [12, 15], [13, 15], [14, 15], [15, 15], [16, 15], [17, 15], [18, 15], [19, 15], [20, 15], [21, 15], [22, 15], [23, 15], [24, 15], [25, 15], [26, 15], [0, 14], [1, 14], [2, 14], [3, 14], [4, 14], [5, 14], [6, 14], [7, 14], [8, 14], [9, 14], [10, 14], [11, 14], [12, 14], [13, 14], [14, 14], [15, 14], [16, 14], [17, 14], [18, 14], [19, 14], [20, 14], [21, 14], [22, 14], [23, 14], [24, 14], [25, 14], [26, 14], [27, 14]]
+        FRIENDLY_MAP = [[0, 13], [1, 13], [2, 13], [3, 13], [4, 13], [5, 13], [6, 13], [7, 13], [8, 13], [9, 13], [10, 13], [11, 13], [12, 13], [13, 13], [14, 13], [15, 13], [16, 13], [17, 13], [18, 13], [19, 13], [20, 13], [21, 13], [22, 13], [23, 13], [24, 13], [25, 13], [26, 13], [27, 13], [1, 12], [2, 12], [3, 12], [4, 12], [5, 12], [6, 12], [7, 12], [8, 12], [9, 12], [10, 12], [11, 12], [12, 12], [13, 12], [14, 12], [15, 12], [16, 12], [17, 12], [18, 12], [19, 12], [20, 12], [21, 12], [22, 12], [23, 12], [24, 12], [25, 12], [26, 12], [2, 11], [3, 11], [4, 11], [5, 11], [6, 11], [7, 11], [8, 11], [9, 11], [10, 11], [11, 11], [12, 11], [13, 11], [14, 11], [15, 11], [16, 11], [17, 11], [18, 11], [19, 11], [20, 11], [21, 11], [22, 11], [23, 11], [24, 11], [25, 11], [3, 10], [4, 10], [5, 10], [6, 10], [7, 10], [8, 10], [9, 10], [10, 10], [11, 10], [12, 10], [13, 10], [14, 10], [15, 10], [16, 10], [17, 10], [18, 10], [19, 10], [20, 10], [21, 10], [22, 10], [23, 10], [
+            24, 10], [4, 9], [5, 9], [6, 9], [7, 9], [8, 9], [9, 9], [10, 9], [11, 9], [12, 9], [13, 9], [14, 9], [15, 9], [16, 9], [17, 9], [18, 9], [19, 9], [20, 9], [21, 9], [22, 9], [23, 9], [5, 8], [6, 8], [7, 8], [8, 8], [9, 8], [10, 8], [11, 8], [12, 8], [13, 8], [14, 8], [15, 8], [16, 8], [17, 8], [18, 8], [19, 8], [20, 8], [21, 8], [22, 8], [6, 7], [7, 7], [8, 7], [9, 7], [10, 7], [11, 7], [12, 7], [13, 7], [14, 7], [15, 7], [16, 7], [17, 7], [18, 7], [19, 7], [20, 7], [21, 7], [7, 6], [8, 6], [9, 6], [10, 6], [11, 6], [12, 6], [13, 6], [14, 6], [15, 6], [16, 6], [17, 6], [18, 6], [19, 6], [20, 6], [8, 5], [9, 5], [10, 5], [11, 5], [12, 5], [13, 5], [14, 5], [15, 5], [16, 5], [17, 5], [18, 5], [19, 5], [9, 4], [10, 4], [11, 4], [12, 4], [13, 4], [14, 4], [15, 4], [16, 4], [17, 4], [18, 4], [10, 3], [11, 3], [12, 3], [13, 3], [14, 3], [15, 3], [16, 3], [17, 3], [11, 2], [12, 2], [13, 2], [14, 2], [15, 2], [16, 2], [12, 1], [13, 1], [14, 1], [15, 1], [13, 0], [14, 0]]
+        self.current_structure_map = [False for pos in FRIENDLY_MAP]
+        self.preemptive_destroying_location = []
+        self.preemptive_rebuilding_location = []
         MP = 1
         SP = 0
         # This is a good place to do initial setup
@@ -79,16 +84,18 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         self.destructive_interceptors_count = 0
         self.enemy_shielding_power = 0
+        self.preemptive_destroying_location = []
         self.cf_preflight_check(game_state)
 
-    def cf_preflight_check(self, game_state):
+    def cf_preflight_check(self, game_state) -> None:
         """
         Analyse the current game conditions:
         1. If enemy has MP > 10, prepare self destructive interceptors
         2. If enemy's total effective shielding amount > 40, prepare more destructive interceptors
-        3. Define the frontline units that should be pre-emptively repaired in this round
-        4. Scan enemy defense line for any new holes to be opened next round
-        5. Conclude enemy defense type: blocked or single-opened or double-opened
+        3. Rebuild the pre-emptively destroyed structures
+        4. Define the frontline units that should be pre-emptively destroyed in this round
+        5. Scan enemy defense line for any new holes to be opened next round
+        6. Conclude enemy defense type: blocked or single-opened or double-opened
         """
 
         # 1. If enemy has MP > 10, prepare self destructive interceptors
@@ -104,10 +111,69 @@ class AlgoStrategy(gamelib.AlgoCore):
                 if unit.unit_type == "EF":
                     self.enemy_shielding_power += self.cf_calculate_support_power(
                         unit.y, unit.upgraded)
+        self.destructive_interceptors_count += (
+            self.enemy_shielding_power+20)//40
         gamelib.debug_write("Enemy has {} effective shielding".format(
             self.enemy_shielding_power))
 
-        # 3. Define the frontline units that should be pre-emptively repaired in this round
+        # 3. Rebuild the pre-emptively destroyed structures
+        for rebuild_unit in self.preemptive_rebuilding_location:
+            gamelib.debug_write("Rebuilding {}".format(rebuild_unit))
+            unit_type = rebuild_unit.unit_type
+            if(unit_type == "FF"):
+                unit_type = WALL
+            elif(unit_type == "EF"):
+                unit_type = SUPPORT
+            elif(unit_type == "DF"):
+                unit_type = TURRET
+            else:
+                game_state.warn(
+                    "Unknown structure type: {}".format(unit_type))
+                continue
+            game_state.attempt_spawn(
+                unit_type, [rebuild_unit.x, rebuild_unit.y])
+            if(rebuild_unit.upgraded):
+                game_state.attempt_upgrade([rebuild_unit.x, rebuild_unit.y])
+
+        # 4. Define the frontline units that should be pre-emptively repaired in this round
+        # load the current structure state before the coming round start
+        game_state.attempt_spawn(WALL, [[5, 10], [6, 10], [5, 11], [7, 12]])
+        game_state.attempt_spawn(TURRET, [[5, 9], [6, 9]])
+        game_state.attempt_upgrade([[5, 10], [6, 10], [5, 11]])
+        game_state.attempt_upgrade([[6, 9]])
+        self.current_structure_map = [game_state.contains_stationary_unit(
+            pos) for pos in FRIENDLY_MAP]
+        for structure in self.current_structure_map:
+            if(structure != False):
+                # remove a structure if its current health is less than 60% of its maximum health
+                maxhealth = 1
+                if(structure.unit_type == "FF"):
+                    if(structure.upgraded):
+                        # source code bug: init a upgraded wall but health is still recorded as 12 in the first round
+                        if(structure.health == 12):
+                            maxhealth = 12
+                        else:
+                            maxhealth = 120
+                    else:
+                        maxhealth = 12
+                elif(structure.unit_type == "EF"):
+                    maxhealth = 30
+                elif(structure.unit_type == "DF"):
+                    maxhealth = 75
+                else:
+                    game_state.warn(
+                        "Unknown structure type: {}".format(structure.unit_type))
+                    continue
+                gamelib.debug_write(structure)
+                gamelib.debug_write(structure.health)
+                gamelib.debug_write(maxhealth)
+                if(structure.health/maxhealth <= 0.6):
+                    self.preemptive_destroying_location.append(structure)
+                    game_state.attempt_remove([structure.x, structure.y])
+        # record the removed structures, rebuild next round
+        self.preemptive_rebuilding_location = self.preemptive_destroying_location
+        gamelib.debug_write(
+            "Pre-emptively destroying {} structures".format(self.preemptive_destroying_location))
 
     def cf_calculate_support_power(self, y: int, upgraded: bool) -> int:
         """
